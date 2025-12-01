d9819f6b9d7b5a09cf2d7e21149c52b72f1d5133
Grover Gonzalez <grovergonzalez8@gmail.com>
2025-12-01 14:19:16 -0400

    agregando el profile en el auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/core/dtos/users/create-user.dto';
+import { LoginUserDto } from 'src/core/dtos/login/login.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    async register(@Body() dto: CreateUserDto) {
+        return this.authService.register(dto);
+    }
+
+    @Post('login')
+    async login(@Body() dto: LoginUserDto) {
+        return this.authService.login(dto);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    async profile(@Request() req) {
+        return req.user;
+    }
+}
