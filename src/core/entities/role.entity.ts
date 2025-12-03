b2a2ad839563c8214a4ec652111400310e64f419
Grover Gonzalez <grovergonzalez8@gmail.com>
2025-12-03 00:12:07 -0400

    creando la entidad de roles

---
+++ src/core/entities/role.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('roles')
+export class Role {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ nullable: true })
+    description?: string;
+}
