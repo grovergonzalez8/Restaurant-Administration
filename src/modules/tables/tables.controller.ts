af947e25af77b719334e94112c4bc1696e68a336
Grover Gonzalez <grovergonzalez8@gmail.com>
2025-12-02 23:55:36 -0400

    agregando guards a tables y users

--- src/modules/tables/tables.controller.ts
+++ src/modules/tables/tables.controller.ts
@@ -1,33 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { TablesService } from './tables.service';
 import { CreateTableDto } from 'src/core/dtos/tables/create-table.dto';
 import { UpdateTableDto } from 'src/core/dtos/tables/update-table.dto';
+import { RolesGuard } from '../auth/roles.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Roles } from '../auth/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('tables')
 export class TablesController {
     constructor(private readonly tablesService: TablesService) {}
 
     @Get()
+    @Roles('admin', 'kitchen', 'waiter')
     findAll() {
         return this.tablesService.findAll();
     }
 
     @Get(':id')
+    @Roles('admin', 'host', 'waiter')
     findOne(@Param('id') id: string) {
         return this.tablesService.findOne(id);
     }
 
     @Post()
+    @Roles('admin')
     create(@Body() dto: CreateTableDto) {
         return this.tablesService.create(dto);
     }
 
     @Put(':id')
+    @Roles('admin')
     update(@Param('id') id: string, @Body() dto: UpdateTableDto) {
         return this.tablesService.update(id, dto);
     }
 
     @Delete(':id')
+    @Roles('admin')
     remove(@Param('id') id: string) {
         return this.tablesService.remove(id);
     }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,34 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from 'src/core/dtos/users/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from 'src/core/dtos/users/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('users')
 export class UsersController {
 
     constructor(private readonly usersService: UsersService) {}
 
     @Get()
+    @Roles('admin')
     findAll() {
         return this.usersService.findAll();
     }
 
     @Get(':id')
+    @Roles('admin')
     findOne(@Param('id') id: string) {
         return this.usersService.findOne(id);
     }
     
     @Post()
+    @Roles('admin')
     create(@Body() dto: CreateUserDto) {
         return this.usersService.create(dto);
     }
 
     @Put(':id')
+    @Roles('admin')
     update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
         return this.usersService.update(id, dto);
     }
 
     @Delete(':id')
+    @Roles('admin')
     remove(@Param('id') id: string) {
         return this.usersService.remove(id);
     }
