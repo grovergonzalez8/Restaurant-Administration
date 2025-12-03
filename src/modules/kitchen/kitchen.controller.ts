5ef7f254275f279c40e42f0ee5a5d10866667910
Grover Gonzalez <grovergonzalez8@gmail.com>
2025-12-03 00:06:56 -0400

    agregando guards en la cocina

--- src/modules/kitchen/kitchen.controller.ts
+++ src/modules/kitchen/kitchen.controller.ts
@@ -1,33 +1,43 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { KitchenService } from './kitchen.service';
 import { CreateKitchenOrderDto } from 'src/core/dtos/kitchen/create-kitchen-order.dto';
 import { UpdateKitchenStatusDto } from 'src/core/dtos/kitchen/update-kitchen-order.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('kitchen')
 export class KitchenController {
     constructor(private readonly kitchenService: KitchenService) {}
 
-    @Post()
-    create(@Body() dto: CreateKitchenOrderDto) {
-        return this.kitchenService.create(dto);
-    }
-
     @Get()
+    @Roles('kitchen', 'admin')
     findAll() {
         return this.kitchenService.findAll();
     }
 
     @Get(':id')
+    @Roles('kitchen', 'admin')
     findOne(@Param('id') id: string) {
         return this.kitchenService.findOne(id);
     }
 
+
+    @Post()
+    @Roles('kitchen', 'admin')
+    create(@Body() dto: CreateKitchenOrderDto) {
+        return this.kitchenService.create(dto);
+    }
+
     @Put(':id/status')
+    @Roles('kitchen', 'admin')
     updateStatus(@Param('id') id: string, @Body() dto: UpdateKitchenStatusDto) {
         return this.kitchenService.updateStatus(id, dto);
     }
 
     @Delete(':id')
+    @Roles('admin')
     remove(@Param('id') id: string) {
         return this.kitchenService.remove(id);
     }
